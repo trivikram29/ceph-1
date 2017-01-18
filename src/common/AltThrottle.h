0362db43fb050cbcc5aeece8f07cd797fc16d85c
J. Eric Ivancich <ivancich@redhat.com>
2017-01-18 12:31:39 -0500

    Add collection of count data (# transactions in queue) in addition to cumulative # ops / byte-count in queue.
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/common/AltThrottle.h
+++ src/common/AltThrottle.h
@@ -37,19 +37,22 @@ public:
 template<typename T>
 class DataCollectionThrottle {
   struct ThrottleTiming {
-    utime_t start;
-    uint64_t count;
-    uint64_t total;
+    utime_t start_time;
+    uint32_t tx_count;
+    uint64_t tx_size;
+    uint64_t tx_total_size;
 
-    ThrottleTiming(uint64_t count, uint64_t total) :
-      start(ceph_clock_now(NULL)),
-      count(count),
-      total(total)
+    ThrottleTiming(uint32_t tx_count, uint64_t tx_size, uint64_t tx_total_size) :
+      start_time(ceph_clock_now(NULL)),
+      tx_count(tx_count),
+      tx_size(tx_size),
+      tx_total_size(tx_total_size)
     { }
   };
 
   std::string name;
   std::map<T,ThrottleTiming> map;
+  uint32_t count;
   uint64_t total;
   Mutex lock;
 
@@ -57,12 +60,14 @@ public:
 
   DataCollectionThrottle(const std::string& name) :
     name(name),
+    count(0),
     total(0),
     lock(name)
   { }
 
   DataCollectionThrottle(const char* name) :
     name(name),
+    count(0),
     total(0),
     lock(name)
   { }
@@ -71,27 +76,30 @@ public:
     return total;
   }
 
-  void get(uint64_t count, T index) {
+  void get(uint32_t tx_size, T index) {
     Mutex::Locker l(lock);
-    total += count;
-    map.emplace(index, ThrottleTiming(count, total));
+    total += tx_size;
+    ++count;
+    map.emplace(index, ThrottleTiming(count, tx_size, total));
   }
 
   ostream& put(T index, ostream& out) {
     utime_t duration;
-    uint64_t count;
+    uint64_t count_at_get;
     uint64_t total_at_get;
     {
       Mutex::Locker l(lock);
       auto it = map.find(index);
-      // assert(it != map.end())
-      count = it->second.count;
-      total -= count;
-      total_at_get = it->second.total;
-      duration = ceph_clock_now(NULL) - it->second.start;
+      assert(it != map.end());
+      --count;
+      count_at_get = it->second.tx_count;
+      total -= it->second.tx_size;
+      total_at_get = it->second.tx_total_size;
+      duration = ceph_clock_now(NULL) - it->second.start_time;
       map.erase(it);
     }
-    out << name << ": (" << total_at_get << "," << duration.to_nsec() << ")";
+    out << name << ": (" << count_at_get << "," << total_at_get <<
+      "," << duration.to_nsec() << ")";
     return out;
   }
 }; // class DataCollectionThrottle
